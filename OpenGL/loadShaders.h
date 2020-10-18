0d04d80c54bba7dbccf64e6bfe32bfe866439818
VladDuncea <vlad_duncea_31@yahoo.com>
2020-10-18 17:00:21 +0300

    Add project files.

---
+++ OpenGL/loadShaders.h
@@ -0,0 +1,4 @@
+#include <GL/glew.h> // glew apare inainte de freeglut
+#include <GL/freeglut.h> // nu trebuie uitat freeglut.h
+
+GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path);
