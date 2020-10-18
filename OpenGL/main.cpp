b040bc17aa8cb83b87b088a717eb168ae8afda34
VladDuncea <vlad_duncea_31@yahoo.com>
2020-10-18 20:12:57 +0300

    Transfered 01_04 in  new OpenGL

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,185 +1,95 @@
-//// Codul sursa este adaptat dupa OpenGLBook.com
+//#include <windows.h>
+//#include <gl/freeglut.h>
 //
-//#include <windows.h>  // biblioteci care urmeaza sa fie incluse
-//#include <stdlib.h> // necesare pentru citirea shader-elor
-//#include <stdio.h>
-//#include <GL/glew.h> // glew apare inainte de freeglut
-//#include <GL/freeglut.h> // nu trebuie uitat freeglut.h
-//
-////////////////////////////////////////
-//
-//GLuint
-//VaoId,
-//VboId,
-//ColorBufferId,
-//VertexShaderId,
-//FragmentShaderId,
-//ProgramId;
-//
-//
-/////////////////////////////////////////////
-//
-//// Shader-ul de varfuri / Vertex shader (este privit ca un sir de caractere)
-//const GLchar* VertexShader =
+//void init(void)  // initializare fereastra de vizualizare
 //{
-//  "#version 400\n"\
+//	glClearColor(1.0, 1.0, 1.0, 0.0); // precizeaza culoarea de fond a ferestrei de vizualizare
 //
-//  "layout(location=0) in vec4 in_Position;\n"\
-//  "layout(location=1) in vec4 in_Color;\n"\
-//  "out vec4 ex_Color;\n"\
-//
-//  "void main(void)\n"\
-//  "{\n"\
-//  "  gl_Position = in_Position;\n"\
-//  "  ex_Color = in_Color;\n"\
-//  "}\n"
-//};
-//// Shader-ul de fragment / Fragment shader (este privit ca un sir de caractere)
-//const GLchar* FragmentShader =
-//{
-//  "#version 400\n"\
-//
-//  "in vec4 ex_Color;\n"\
-//  "out vec4 out_Color;\n"\
-//
-//  "void main(void)\n"\
-//  "{\n"\
-//  "  out_Color = ex_Color;\n"\
-//  "}\n"
-//};
-//
-//void CreateVBO(void)
-//{
-//    // varfurile 
-//    GLfloat Vertices[] = {
-//      (100-800.0)/800,(10-700.0)/700 , 0.0f, 1.0f,
-//      (200 - 800.0) / 800,(120 - 700.0) / 700, 0.0f, 1.0f,
-//      (155 - 800.0) / 800,(290 - 700.0) / 700, 0.0f, 1.0f,
-//
-//      (250 - 800.0) / 800,(300 - 700.0) / 700 , 0.0f, 1.0f,
-//      (50 - 800.0) / 800,(300 - 700.0) / 700, 0.0f, 1.0f,
-//      (200 - 800.0) / 800,(350 - 700.0) / 700, 0.0f, 1.0f,
-//    };
-//
-//    // culorile, ca atribute ale varfurilor
-//    GLfloat Colors[] = {
-//      1.0f, 0.0f, 0.0f, 1.0f,
-//      0.0f, 1.0f, 0.0f, 1.0f,
-//      0.0f, 0.0f, 1.0f, 1.0f,
-//      1.0f, 1.0f, 0.0f, 1.0f,
-//      1.0f, 1.0f, 0.0f, 1.0f,
-//      1.0f, 0.0f, 1.0f, 1.0f
-//    };
-//
-//
-//    // se creeaza un buffer nou
-//    glGenBuffers(1, &VboId);
-//    // este setat ca buffer curent
-//    glBindBuffer(GL_ARRAY_BUFFER, VboId);
-//    // punctele sunt "copiate" in bufferul curent
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-//
-//    // se creeaza / se leaga un VAO (Vertex Array Object) - util cand se utilizeaza mai multe VBO
-//    glGenVertexArrays(1, &VaoId);
-//    glBindVertexArray(VaoId);
-//    // se activeaza lucrul cu atribute; atributul 0 = pozitie
-//    glEnableVertexAttribArray(0);
-//    // 
-//    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-//
-//    // un nou buffer, pentru culoare
-//    glGenBuffers(1, &ColorBufferId);
-//    glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
-//    // atributul 1 =  culoare
-//    glEnableVertexAttribArray(1);
-//    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
-//
-//
-//}
-//void DestroyVBO(void)
-//{
-//
-//
-//    glDisableVertexAttribArray(1);
-//    glDisableVertexAttribArray(0);
-//
-//    glBindBuffer(GL_ARRAY_BUFFER, 0);
-//    glDeleteBuffers(1, &ColorBufferId);
-//    glDeleteBuffers(1, &VboId);
-//
-//    glBindVertexArray(0);
-//    glDeleteVertexArrays(1, &VaoId);
-//
-//
+//	glMatrixMode(GL_PROJECTION);  // se precizeaza este vorba de o reprezentare 2D, realizata prin proiectie ortogonala
+//	gluOrtho2D(0, 800.0, 0.0, 700.0); // sunt indicate coordonatele extreme ale ferestrei de vizualizare
 //}
 //
-//void CreateShaders(void)
+//void desen(void) // procedura desenare  
 //{
+//	// dreptunghi: desenare directa
+//	glColor3f(0.0, 0.0, 1.0);
+//	glRecti(20, 130, 140, 310);
 //
-//    VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-//    glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
-//    glCompileShader(VertexShaderId);
 //
-//    FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-//    glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
-//    glCompileShader(FragmentShaderId);
+//	//poligon convex
+//	glColor3f(1.0, 0.0, 0.0);
+//	glBegin(GL_POLYGON);
+//	glVertex2i(0, 0);
+//	glVertex2i(100, 10);
+//	glVertex2i(200, 120);
+//	glVertex2i(155, 290);
+//	glEnd();
 //
-//    ProgramId = glCreateProgram();
-//    glAttachShader(ProgramId, VertexShaderId);
-//    glAttachShader(ProgramId, FragmentShaderId);
-//    glLinkProgram(ProgramId);
-//    glUseProgram(ProgramId);
 //
-//}
-//void DestroyShaders(void)
-//{
 //
-//    glUseProgram(0);
 //
-//    glDetachShader(ProgramId, VertexShaderId);
-//    glDetachShader(ProgramId, FragmentShaderId);
+//	// evantai de triunghiuri
+//	glColor3d(1.0, 0.0, 0.25);
+//	glBegin(GL_TRIANGLE_FAN);
+//	glVertex2i(250, 300);
+//	glVertex2i(50, 300);
+//	glVertex2i(200, 350);
+//	glVertex2i(250, 500);
+//	glVertex2i(350, 200);
+//	glEnd();
 //
-//    glDeleteShader(FragmentShaderId);
-//    glDeleteShader(VertexShaderId);
+//	// reuniune de triunghiuri
+//	glColor3d(1.0, 0.75, 0.25);
+//	glBegin(GL_TRIANGLE_STRIP);
+//	glVertex2i(650, 300);
+//	glVertex2i(450, 300);
+//	glVertex2i(600, 350);
+//	glVertex2i(650, 500);
+//	glVertex2i(750, 200);
+//	glEnd();
 //
-//    glDeleteProgram(ProgramId);
-//
-//}
-//
-//void Initialize(void)
-//{
-//    glClearColor(1.0f, 1.0f, 1.0f, 0.0f); // culoarea de fond a ecranului
-//}
-//void RenderFunction(void)
-//{
-//    glClear(GL_COLOR_BUFFER_BIT);
-//    CreateVBO();
-//    CreateShaders();
-//    glPointSize(20.0);
-//    glDrawArrays(GL_POINTS, 0, 3);
-//    glDrawArrays(GL_TRIANGLES, 3, 3);
-//    glFlush();
-//}
-//void Cleanup(void)
-//{
-//    DestroyShaders();
-//    DestroyVBO();
+//	glFlush(); // proceseaza procedurile OpenGL cat mai rapid
 //}
 //
-//int main(int argc, char* argv[])
+//void desen2(void)
 //{
 //
-//    glutInit(&argc, argv);
-//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-//    glutInitWindowPosition(100, 100); // pozitia initiala a ferestrei
-//    glutInitWindowSize(700, 500); //dimensiunile ferestrei
-//    glutCreateWindow("Primul triunghi - OpenGL <<nou>>"); // titlul ferestrei
-//    glewInit(); // nu uitati de initializare glew; trebuie initializat inainte de a a initializa desenarea
-//    Initialize();
-//    glutDisplayFunc(RenderFunction);
-//    glutCloseFunc(Cleanup);
-//    glutMainLoop();
+//	glColor3f(1, 0, 0);
+//	glRecti(-30, 20, 40, 10);
+//	// 
+//	glColor3f(1, 1, 0);
+//	glMatrixMode(GL_MODELVIEW);
+//	glPushMatrix();
+//	glTranslatef(-10.0, -20.0, 0.0);
+//	glScalef(0.5, 2.0, 0.0);
+//	glRecti(-30, 20, 40, 10);
+//	glPopMatrix();
 //
+//	glPointSize(4.0);
+//	glColor3f(0, 0, 0);
+//	glBegin(GL_POINTS);
+//	glVertex3f(40, 20, 0);
+//	glEnd();
 //
+//	glPointSize(8.0);
+//	glColor3f(0, 0, 0);
+//	glBegin(GL_POINTS);
+//	glVertex3f(10, 20, 0);
+//	glEnd();
+//
+//	glFlush();
+//
+//}
+//void main(int argc, char** argv)
+//{
+//	glutInit(&argc, argv); // initializare GLUT
+//	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // se utilizeaza un singur buffer | modul de colorare RedGreenBlue (= default)
+//	glutInitWindowPosition(100, 100); // pozitia initiala a ferestrei de vizualizare (in coordonate ecran)
+//	glutInitWindowSize(800, 600); // dimensiunile ferestrei 
+//	glutCreateWindow("Poligoane"); // creeaza fereastra, indicand numele ferestrei de vizualizare - apare in partea superioara
+//
+//	init(); // executa procedura de initializare
+//	glClear(GL_COLOR_BUFFER_BIT); // reprezentare si colorare fereastra de vizualizare
+//	glutDisplayFunc(desen); // procedura desen este invocata ori de cate ori este nevoie
+//	glutMainLoop(); // ultima instructiune a programului, asteapta (eventuale) noi date de intrare
 //}
